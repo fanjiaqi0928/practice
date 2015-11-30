d63c3c46de464c4697ae081199856d395adb15f9
fanjiaqi <fanjiaqi@gyyx.cn>
2015-11-30 10:05:21 +0800

    project framework

---
+++ cn.gyyx.demo/demo-root/demo-beans/src/main/java/gyyx/cn/demo/beans/App.java
@@ -0,0 +1,13 @@
+package gyyx.cn.demo.beans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cn.gyyx.demo/demo-root/demo-bll/src/main/java/gyyx/cn/demo/bll/App.java
@@ -0,0 +1,13 @@
+package gyyx.cn.demo.bll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
